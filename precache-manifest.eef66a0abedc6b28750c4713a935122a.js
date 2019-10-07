808d5cedc82e06fd3db25b6469ca81b0e3291ab8
kurniad-sign <agungkurniadi99@gmail.com>
2019-10-07 15:41:43 +0700

    Updates

---
+++ precache-manifest.eef66a0abedc6b28750c4713a935122a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89be9f74265c38aaee228d81d1076c95",
+    "url": "/simple-calculator/index.html"
+  },
+  {
+    "revision": "61c9f69a585f0a8045f8",
+    "url": "/simple-calculator/static/css/main.df4e896b.chunk.css"
+  },
+  {
+    "revision": "8ce242a5a3862ad492c9",
+    "url": "/simple-calculator/static/js/2.54f27ce6.chunk.js"
+  },
+  {
+    "revision": "61c9f69a585f0a8045f8",
+    "url": "/simple-calculator/static/js/main.c15d17d7.chunk.js"
+  },
+  {
+    "revision": "0b0fdf8ea3fba550e756",
+    "url": "/simple-calculator/static/js/runtime-main.9e9452c7.js"
+  }
+]);
